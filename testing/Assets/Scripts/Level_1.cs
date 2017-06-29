7a53053761bb3807adb8bdbfc4e9e2a0c43911f3
Micky Yu <mickyyu96@gmail.com>
2017-06-29 20:38:24 +0700

    initial commit

---
+++ testing/Assets/Scripts/Level_1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_1 : MonoBehaviour {
+
+    public GameObject objectButtonPrefab;
+    public GameObject objectButtonContainer;
+
+    private void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Level_1");
+        foreach (Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(objectButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(objectButtonContainer.transform, false);
+        }
+    }
+}
---
+++ testing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject levelButtonPrefab;
+    public GameObject levelButtonContainer;
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+        foreach(Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(levelButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(levelButtonContainer.transform, false);
+
+            string sceneName = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+        }
+    }
+
+    private void Update()
+    {
+        if (cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, 5 * Time.deltaTime);
+        }
+    }
+
+    private void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+        cameraDesiredLookAt = menuTransform;
+    }
+}
